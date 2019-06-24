790eaababa01afe69850bbf69318a6608fa93353
irakli15 <imghe15@freeuni.edu.ge>
2019-06-24 20:49:59 +0400

    added initial disk_inode structure, probably will be changed in future for adding permissions and other attributes

--- inode.h
+++ inode.h
@@ -1,5 +1,14 @@
 #define SECTOR_SIZE 1024
-
+#define DBL_IND_COUNT 16
+#define DIRECT_COUNT 236
 typedef unsigned long long inumber_t;
 typedef unsigned int sector_t;
 typedef unsigned int uint_size_t;
+
+struct disk_inode{
+  inumber_t inumber; //8
+  uint_size_t length; //4
+  sector_t direct[DIRECT_COUNT]; //
+  sector_t ind; //4
+  sector_t dbl_ind[DBL_IND_COUNT]; //16*4
+};
